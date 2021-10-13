ef3c0db9c75be03db032d227fb56cb3ec28db5b6
domi555 <66876360+domi555@users.noreply.github.com>
2021-10-13 13:29:53 +0200

    Update

---
+++ client/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div> -->
+
+  <!-- <div id="app">
+    <AppButton>Home</AppButton>
+    <AppButton>About</AppButton>
+  </div> -->
+
+  <div id="app">
+    <AppButton><img src="home.svg" /> Home</AppButton>
+    <br />
+    <AppButton>About <img src="info.svg" /> Me</AppButton>
+  </div>
+</template>
+
+<script>
+import AppButton from '@/components/AppButton.vue';
+
+export default {};
+</script>
+
+<style scoped>
+button {
+  padding: 5px;
+  border: 1px solid black;
+  border-radius: 10px;
+  text-transform: uppercase;
+  width: 150px;
+  height: 185px;
+  background-color: white;
+}
+</style>
---
+++ client/src/components/AppButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button><slot /></button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+button {
+  padding: 5px;
+  border: 1px solid black;
+  border-radius: 10px;
+  text-transform: uppercase;
+  width: 150px;
+  height: 185px;
+  background-color: white;
+}
+</style>
