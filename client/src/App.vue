1eb0b612cc4148f566e55ecb0435cb08b6d6203c
domi555 <66876360+domi555@users.noreply.github.com>
2021-10-13 14:05:39 +0200

    Update

--- client/src/App.vue
+++ client/src/App.vue
@@ -12,17 +12,58 @@
     <AppButton>About</AppButton>
   </div> -->
 
-  <div id="app">
+  <!-- <div id="app">
     <AppButton><img src="home.svg" /> Home</AppButton>
     <br />
     <AppButton>About <img src="info.svg" /> Me</AppButton>
+  </div> -->
+
+  <!-- <div id="app">
+    <AppButton @click="handle"><img src="home.svg" /> Home</AppButton>
+    <br />
+    <AppButton>About <img src="info.svg" /> Me</AppButton>
+  </div> -->
+
+  <!-- AUFGABE (1) -->
+  <!-- <div style="display:flex">
+    <Sheet style="margin-left:20px">
+      <h3>Memo to myself</h3>
+      <p>Always prepare for PLFs in time!</p>
+      <img src="info.svg" width="80px" />
+    </Sheet>
+    <Sheet style="margin-left:20px">
+      <p>INSY, SEW and Webtechnologies are our favorite subjects!</p>
+      <span style="font-style: italic">Starkschwitz Fiedler, Jan Schreck</span>
+      <p>Wish i was there!</p>
+      <span style="font-style: italic">E. Wahn</span>
+    </Sheet>
+  </div> -->
+
+  <div style="display:flex">
+    <Sheet style="margin-left:20px" bg-color="blue" color="white" width="250" height="300">
+      <h3>Memo to myself</h3>
+      <p>Always prepare for PLFs in time!</p>
+      <img src="info.svg" width="80px" />
+    </Sheet>
+    <Sheet style="margin-left:20px" bg-color="purple" color="yellow" width="400" height="300">
+      <p>INSY, SEW and Webtechnologies are our favorite subjects!</p>
+      <span style="font-style: italic">Starkschwitz Fiedler, Jan Schreck</span>
+      <p>Wish is was there!</p>
+      <span style="font-style: italic">E. Wahn</span>
+    </Sheet>
   </div>
 </template>
 
 <script>
-import AppButton from '@/components/AppButton.vue';
+// import AppButton from '@/components/AppButton.vue';
+import Sheet from '@/components/Sheet.vue';
 
-export default {};
+export default {
+  components: {
+    // AppButton,
+    Sheet,
+  },
+};
 </script>
 
 <style scoped>
--- client/src/components/Sheet.vue
+++ client/src/components/Sheet.vue
@@ -1,9 +1,30 @@
 <template>
-  <div></div>
+  <div v-on="$listeners" class="slot-style"><slot /></div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ['bg-color', 'color', 'width', 'height'],
+  computed: {
+    style() {
+      return `background-color: ${this.bgColor}; color: ${this.color}; width: ${this.width}; height: ${this.height};`;
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.slot-style {
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  font-family: Arial;
+  text-align: center;
+  padding: 15px;
+  width: 300px;
+  height: 300px;
+  box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2),
+    0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12);
+}
+</style>
